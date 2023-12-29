c12f9f7ae815e6bb5407182f9328eb0953b24fbb
Tejas Bhandare <tejasbhandare8329@gmail.com>
2023-12-29 22:44:56 +0530

    29Dec

---
+++ Assignment6/Application3/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template:
+    `<h1>Marvellous Infosystems</h1><input type="text">
+    <router-outlet></router-outlet>`,
+  styles:
+    `h1{color : blue;}`
+})
+export class AppComponent {
+  title = 'Application3';
+}
